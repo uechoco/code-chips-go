417b4826a27544b2d89812febf6364bd60647113
uechoco <uechoco@gmail.com>
2019-08-21 15:40:01 +0900

    added How To Write Go Code

---
+++ HowToWriteGoCode/README.md
@@ -0,0 +1,2 @@
+How to Write Go Code - The Go Programming Language
+https://tip.golang.org/doc/code.html
---
+++ HowToWriteGoCode/newmath/sqrt_test.go
@@ -0,0 +1,24 @@
+package newmath
+
+import "testing"
+
+func TestSqrt(t *testing.T) {
+	type args struct {
+		x float64
+	}
+	tests := []struct {
+		name string
+		args args
+		want float64
+	}{
+		{"整数4", args{4}, 2},
+		{"整数9", args{9}, 3},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Sqrt(tt.args.x); got != tt.want {
+				t.Errorf("Sqrt() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
